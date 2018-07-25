a43791e7786593678a2e9b8e38337eac44d767e0
Mark <hollyfeldl@gmail.com>
2018-07-24 20:54:40 -0700

    remove ";" per style guide

--- values.groovy
+++ values.groovy
@@ -1,10 +1,10 @@
 // values example
 // https://gobyexample.com/values
 
-println "groovy" + "lang";
-println "1+1 = ${ 1 + 1 }";
-println "7.0/3.0 = ${ 7.0 / 3.0 }";
-println "${ true && false }";
-println "${ true || false }";
-println "${ !true }";
+println "groovy" + "lang"
+println "1+1 = ${ 1 + 1 }"
+println "7.0/3.0 = ${ 7.0 / 3.0 }"
+println "${ true && false }"
+println "${ true || false }"
+println "${ !true }"
 
--- variables.groovy
+++ variables.groovy
@@ -1,19 +1,19 @@
 // the variables example
 // https://gobyexample.com/variable
 
-String a = "initial";
-println a;
+String a = "initial"
+println a
 
-int b = 1, c = 2;
-println "${b} ${c}";
+int b = 1, c = 2
+println "${b} ${c}"
 
-Boolean d = true;
-println d;
+Boolean d = true
+println d
 
-int e;
-println e;
+int e
+println e
 
 // multiple assignment
 
-def (w,x,y,z) = [1, 3.14, true, "short"];
-println "Multiple Assignment: ${w}, ${x}, ${y}, ${z}";
+def (w,x,y,z) = [1, 3.14, true, "short"]
+println "Multiple Assignment: ${w}, ${x}, ${y}, ${z}"
