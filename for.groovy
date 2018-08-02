23aa4bdbfc8cfb1dc17edc76f24ec44c4c1175b4
Mark <hollyfeldl@gmail.com>
2018-08-01 21:19:31 -0700

    get rid of the class fluff

--- for.groovy
+++ for.groovy
@@ -3,45 +3,42 @@
 
 // while go only has the for loop, groovy has while, for, and for-in
 
-class Example {
 
-	static void main() {
+def Main() {
 
-		// while loop
-		def i = 1
-		while ( i <= 3 ) {
-			println i
-			i = i + 1
+	// while loop
+	def i = 1
+	while ( i <= 3 ) {
+		println i
+		i = i + 1
+	}
+
+	// standard for loop
+	for ( def j = 7; j <= 9; j++ ) {
+		println j
+	}
+
+	// example of a range
+	for ( k in 4..6 ) {
+		println k
+	}
+
+	// make an infinite loop but break out after the first run
+	final Integer q = 1 
+	while ( q == 1 ) {
+		println "loop"
+		break
+	}
+
+	// print only the odd ones
+	for ( def n = 0; n <= 5; n++ ) {
+		if ( n % 2 == 0 ) {
+			continue
 		}
-
-		// standard for loop
-		for ( def j = 7; j <= 9; j++ ) {
-			println j
-		}
-
-		// example of a range
-		for ( k in 4..6 ) {
-			println k
-		}
-
-		// make an infinite loop but break out after the first run
-		final Integer q = 1 
-		while ( q == 1 ) {
-			println "loop"
-			break
-		}
-
-		// print only the odd ones
-		for ( def n = 0; n <= 5; n++ ) {
-			if ( n % 2 == 0 ) {
-				continue
-			}
-			println n
-		}
-
+		println n
 	}
 
 }
 
 // run it 
-Example.main()
+Main()
