7efcba75d3e49777c40a8df438658dea851ab730
Mark <hollyfeldl@gmail.com>
2018-08-11 14:53:59 -0700

    refactor arrays and original slices

--- arrays.groovy
+++ arrays.groovy
@@ -16,37 +16,20 @@ static main() {
 	println "get: ${ a[4] }"
 	println "len: ${ a.size() }"
 
-	// Those that are groovy, will just use lists.
-
-	def l = [null] * 5 // a list of 5 nulls -- only works in one dimension
-	println "is array: ${ l.class.array} "	
-	println "emp: $l"
-
-	l[4] = 101
-	println "set: $l"
-	println "get: ${ l[4] }"
-	println "len: ${ l.size() }"
-
-	def b = [1, 2, 3, 4, 5]
+	def b = [1, 2, 3, 4, 5] as int[]
 	println "dcl: $b"
 
 	// can do the same thing in multiple dimensions
 
-	def twoDList = [null] * 2 // need to build list this way 
-	for ( def init in 0 ..< (twoDList.size()) ) {
-		twoDList[init] = [null] * 3 
-	}
 
 	def twoDArr = new Integer [2][3] // array
 
 	for ( def i in 0..<2 ) {
 		for ( def j in 0..<3 ) {
-			twoDList[i][j] = i + j
 			twoDArr[i][j] = i + j
 		}
 	}
 
-	println "twoDList: $twoDList"
 	println "towDArr: $twoDArr"
 
 }
